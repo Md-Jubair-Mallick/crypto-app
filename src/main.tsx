6bd0a4c32c9e5624d49664fd1eeabf237cbe15ab
MD Jubair <mallickblogger@gmail.com>
2025-05-28 20:41:32 +0600

    react-redux setup & create store - react-redux & @reduxjs/toolkit

--- src/main.tsx
+++ src/main.tsx
@@ -1,17 +1,22 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+import { Provider } from 'react-redux'
+
+import './index.css'
+import App from './App.tsx'
+import { store } from './app/store.js'
 
 const queryClient = new QueryClient();
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <QueryClientProvider client={queryClient}>
-    <App />
-    <ReactQueryDevtools />
+      <Provider store={store}>
+        <App />
+      </Provider>
+      <ReactQueryDevtools />
     </QueryClientProvider>
   </StrictMode>,
 )
